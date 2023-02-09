30ea8f3407be74f8b6f5235eefb28b3493df7833
Tuan Anh Nguyen <ntanh3108@gmail.com>
2023-02-09 22:29:01 +0700

    Fix direction

---
+++ src/App1.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Pages from "./pages/Pages"
+import Category from './components/Category';
+import { BrowserRouter } from 'react-router-dom';
+import Search from './components/Search';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { GiKnifeFork } from 'react-icons/gi';
+import Footer from './components/Footer';
+
+function App1() {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Nav>
+                    <GiKnifeFork />
+                    <Logo to={"/"}>
+                        Mlem Mlem
+                    </Logo>
+                </Nav>
+                <Search />
+                <Category />
+                <Pages />
+                <Footer />
+            </BrowserRouter>
+        </div>
+    );
+}
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  align-items: center;
+  font-family: 'Lobster Two', cursive;
+`
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  
+  justify-content: flex-start;
+  text-align: center;
+  align-items: center;
+  svg{
+    font-size: 2rem;
+  }
+`
+
+export default App1;
